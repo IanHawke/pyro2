f830633dc31c64c71380083767bbeb57a2aa841f
Ian Hawke <I.Hawke@soton.ac.uk>
2017-09-05 11:45:37 +0100

    Use higher order WENO schemes.
    Now goes up to WENO9 (k=5), with the results illustrating that order of
    accuracy isn't everything.
    
    Note that in past hydro tests I have seen (and have seen in other's
    results) noticeably larger oscillations with WENO7 (k=4), so whilst this
    is included, it's not recommended.

--- advection_weno/simulation.py
+++ advection_weno/simulation.py
@@ -9,11 +9,38 @@ import advection_weno.fluxes as flx
 import mesh.patch as patch
 import mesh.integration as integration
 import mesh.array_indexer as ai
+from simulation_null import NullSimulation, grid_setup, bc_setup
 
 from util import profile
 
 
 class Simulation(advection.Simulation):
+
+    def initialize(self):
+        """
+        Initialize the grid and variables for advection and set the initial
+        conditions for the chosen problem.
+
+        Over-ridden from the base class as the number of ghosts depends on
+        the WENO order
+        """
+
+        weno_order = self.rp.get_param("advection.weno_order")
+        assert(weno_order in (2, 3, 4, 5)), "Currently only implemented weno_order=2, 3, 4, 5"
+
+        my_grid = grid_setup(self.rp, ng=weno_order+1)
+
+        # create the variables
+        my_data = patch.CellCenterData2d(my_grid)
+        bc = bc_setup(self.rp)[0]
+        my_data.register_var("density", bc)
+        my_data.create()
+
+        self.cc_data = my_data
+
+        # now set the initial conditions for the problem
+        problem = importlib.import_module("advection.problems.{}".format(self.problem_name))
+        problem.init_data(self.cc_data, self.rp)
 
     def substep(self, myd):
         """
